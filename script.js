a8fe4105e212b2c191da60c6cd74b3eeec98104b
Nathan <Bowers.Nathanx@gmail.com>
2023-02-12 10:12:57 -0500

    make numbers input run through and give cal

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 const numbers= document.querySelectorAll('.numbers')
-const problem= []
+const operators= document.querySelectorAll('.operators')
+let problem= []
 
 
 
 function number(e){
 console.log(e.target.textContent)
+problem += e.target.textContent
+console.log(problem)
+if (e.target.textContent == "="){
+    problem =problem.split('+')
+    problem[1]= problem[1].replace('=','')
+    console.log(problem[1])
+    add(problem)
+
+}
 }
 
-function add(num1,num2) {
-    return num1+num2
+function add(problem) {
+    problem=parseFloat(problem[0])+parseFloat(problem[1])
+    console.log(problem)
 }
 function sub(num1,num2) {
     return num1-num2
@@ -30,6 +41,8 @@ function operate(num1, operator, num2){
 }
 function addListeners(){
     numbers.forEach(numberButton=>
-    numberButton.addEventListener('click', number)
+    numberButton.addEventListener('click', number))
+    operators.forEach(operatorButton=>
+       operatorButton.addEventListener('click', number)
 )}
 addListeners()
